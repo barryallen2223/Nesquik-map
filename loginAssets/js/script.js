1bc9177be43b88f1cae95e77f759b4abc8343728
David Madrid <68928426+barryallen2223@users.noreply.github.com>
2023-04-20 17:20:39 -0500

    Add files via upload

--- loginAssets/js/script.js
+++ loginAssets/js/script.js
@@ -1,1 +1,21 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
+const email = document.getElementById('username');
+const pwd = document.getElementById('pwd');
+const logBtn = document.getElementById('loginBtn');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+logBtn.addEventListener('click', () => {
+    if (email.value == "admin" && pwd.value == "1234") {
+        window.location.href = "../menu/index.html";
+    }
+});
