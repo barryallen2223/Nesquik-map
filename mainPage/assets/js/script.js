55b2dd7c38b1785f0914df5a19a46f226071c083
BarryAllen <barryallen@pansito.hacker>
2023-04-26 21:08:25 -0500

    Modifying the user's picture based on their privileges

--- mainPage/assets/js/script.js
+++ mainPage/assets/js/script.js
@@ -1,7 +1,14 @@
 window.addEventListener('load', function () {
     const userName = localStorage.getItem('userName');
-    const h3Element = document.querySelector('.user-info h3');
-    h3Element.textContent = userName;
+    const userType = localStorage.getItem('userType');
+    if (userType == 'admin') {
+        document.getElementById('userPicType').src = 'assets/images/adminUser.png';
+        const h3Element = document.querySelector('.user-info h3');
+        h3Element.textContent = userName + ' (admin)';
+    } else {
+        const h3Element = document.querySelector('.user-info h3');
+        h3Element.textContent = userName;
+    }
     const reloaded = localStorage.getItem('reloaded');
     if (!reloaded) {
         localStorage.setItem('reloaded', 'true');
